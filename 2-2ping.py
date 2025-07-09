77cfb09ad183b9323e7dcff573d6839f745feecf
czsmith132 <chris@islandz.net>
2025-07-09 15:02:36 -0700

    Simplify 2.2ping.py

--- 2-2ping.py
+++ 2-2ping.py
@@ -13,13 +13,7 @@ from devices_import import *
 #  "device_type": 'cisco_nxos_ssh',
 #  "session_log": 'log_NXOS1.txt'
 #}
-#NXOS2={
-#  "host": 'nxos2.lasthop.io',
-#  "username": 'pyclass',  
-#  "password": passwd,
-#  "device_type": 'cisco_nxos_ssh',
-#  "session_log": 'log_NXOS2.txt'
-#}
+#NXOS2={...}
 #cisco4={...}
 
 devices=[cisco4]
@@ -30,15 +24,16 @@ devices=[cisco4]
 #  print(net_connect.find_prompt())
 
 
-#show version
+#extended ping
 net_connect = ConnectHandler(**cisco4)
-output=net_connect.send_command('ping',expect_string=r':')
-output+=net_connect.send_command('\n',expect_string=r':')
-output+=net_connect.send_command('8.8.8.8',expect_string=r':')
-output+=net_connect.send_command('\n',expect_string=r':')
-output+=net_connect.send_command('\n',expect_string=r':')
-output+=net_connect.send_command('\n',expect_string=r':')
-output+=net_connect.send_command('\n',expect_string=r':')
-output+=net_connect.send_command('\n',expect_string=r':')
+net_connect.send_command('ping',expect_string=r':')
+net_connect.send_command('\n',expect_string=r':')
+net_connect.send_command('8.8.8.8',expect_string=r':')
+net_connect.send_command('\n',expect_string=r':')
+net_connect.send_command('\n',expect_string=r':')
+net_connect.send_command('\n',expect_string=r':')
+net_connect.send_command('\n',expect_string=r':')
+#execution=net_connect.send_command('\n',expect_string=r':')
+print(net_connect.send_command('\n',expect_string=r':'))
 
-print(output)
+#print(execution)
